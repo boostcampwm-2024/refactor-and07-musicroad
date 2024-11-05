e6c7900ef43b7141df62325b474248aedfebeed4
Ju Yungyeom <joo_930@naver.com>
2024-11-06 00:12:57 +0900

    [feature] 초기 위치 설정

--- app/src/main/java/com/squirtles/musicroad/map/MapFragment.kt
+++ app/src/main/java/com/squirtles/musicroad/map/MapFragment.kt
@@ -1,18 +1,24 @@
 package com.squirtles.musicroad.map
 
+import android.Manifest
 import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.graphics.toArgb
+import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.CircleOverlay
+import com.naver.maps.map.overlay.LocationOverlay
 import com.naver.maps.map.overlay.OverlayImage
 import com.naver.maps.map.util.FusedLocationSource
 import com.squirtles.musicroad.R
@@ -20,12 +26,13 @@ import com.squirtles.musicroad.databinding.FragmentMapBinding
 import com.squirtles.musicroad.ui.theme.Primary
 import com.squirtles.musicroad.ui.theme.Purple15
 
-
 class MapFragment : Fragment(), OnMapReadyCallback {
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
     private lateinit var naverMap: NaverMap
     private lateinit var locationSource: FusedLocationSource
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationOverlay: LocationOverlay
     private val circleOverlay = CircleOverlay()
 
     override fun onCreateView(
@@ -33,6 +40,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentMapBinding.inflate(inflater, container, false)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
         locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
         return binding.root
     }
@@ -50,20 +58,39 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(naverMap: NaverMap) {
         this.naverMap = naverMap
-        setLocationOverlay()
-        naverMap.addOnLocationChangeListener { location ->
-            setCircleOverlay(location)
+        initLocationOverlay()
+        setInitLocation()
+        setLocationChangeListener()
+    }
+
+    private fun setInitLocation() {
+        if (checkSelfPermission()) {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    locationOverlay.position = LatLng(location)
+                    setCircleOverlay(location)
+                    naverMap.moveCamera(CameraUpdate.scrollTo(LatLng(location)))
+                }
+            }
         }
     }
 
-    private fun setLocationOverlay() {
+    private fun initLocationOverlay() {
         naverMap.locationSource = locationSource
         naverMap.uiSettings.isLocationButtonEnabled = true
         naverMap.locationTrackingMode = LocationTrackingMode.Follow
 
-        val locationOverlay = naverMap.locationOverlay
+        locationOverlay = naverMap.locationOverlay
         locationOverlay.isVisible = true
         locationOverlay.icon = OverlayImage.fromResource(R.drawable.ic_location)
+
+        naverMap.moveCamera(CameraUpdate.zoomTo(INITIAL_CAMERA_ZOOM))
+    }
+
+    private fun setLocationChangeListener() {
+        naverMap.addOnLocationChangeListener { location ->
+            setCircleOverlay(location)
+        }
     }
 
     private fun setCircleOverlay(location: Location) {
@@ -75,8 +102,20 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         circleOverlay.map = naverMap
     }
 
+    private fun checkSelfPermission(): Boolean {
+        return PermissionChecker.checkSelfPermission(requireContext(), PERMISSIONS[0]) ==
+                PermissionChecker.PERMISSION_GRANTED &&
+                PermissionChecker.checkSelfPermission(requireContext(), PERMISSIONS[1]) ==
+                PermissionChecker.PERMISSION_GRANTED
+    }
+
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
         private const val CIRCLE_RADIUS_METER = 100.0
+        private const val INITIAL_CAMERA_ZOOM = 16.5
+        private val PERMISSIONS = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
     }
 }
