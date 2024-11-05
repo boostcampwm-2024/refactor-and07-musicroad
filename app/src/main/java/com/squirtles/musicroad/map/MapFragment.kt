ac8209a40f8c211ec4deca78ba0fde4af947280b
Ju Yungyeom <joo_930@naver.com>
2024-11-05 22:59:47 +0900

    [feature] 내 위치 주변 반경 표시

--- app/src/main/java/com/squirtles/musicroad/map/MapFragment.kt
+++ app/src/main/java/com/squirtles/musicroad/map/MapFragment.kt
@@ -1,23 +1,32 @@
 package com.squirtles.musicroad.map
 
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.graphics.toArgb
 import androidx.fragment.app.Fragment
+import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.CircleOverlay
+import com.naver.maps.map.overlay.OverlayImage
 import com.naver.maps.map.util.FusedLocationSource
+import com.squirtles.musicroad.R
 import com.squirtles.musicroad.databinding.FragmentMapBinding
+import com.squirtles.musicroad.ui.theme.Primary
+import com.squirtles.musicroad.ui.theme.Purple15
 
 
 class MapFragment : Fragment(), OnMapReadyCallback {
-    private lateinit var locationSource: FusedLocationSource
-    private lateinit var naverMap: NaverMap
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
+    private lateinit var naverMap: NaverMap
+    private lateinit var locationSource: FusedLocationSource
+    private val circleOverlay = CircleOverlay()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,12 +50,33 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(naverMap: NaverMap) {
         this.naverMap = naverMap
+        setLocationOverlay()
+        naverMap.addOnLocationChangeListener { location ->
+            setCircleOverlay(location)
+        }
+    }
+
+    private fun setLocationOverlay() {
         naverMap.locationSource = locationSource
         naverMap.uiSettings.isLocationButtonEnabled = true
         naverMap.locationTrackingMode = LocationTrackingMode.Follow
+
+        val locationOverlay = naverMap.locationOverlay
+        locationOverlay.isVisible = true
+        locationOverlay.icon = OverlayImage.fromResource(R.drawable.ic_location)
+    }
+
+    private fun setCircleOverlay(location: Location) {
+        circleOverlay.center = LatLng(location.latitude, location.longitude)
+        circleOverlay.color = Purple15.toArgb()
+        circleOverlay.outlineColor = Primary.toArgb()
+        circleOverlay.outlineWidth = 3
+        circleOverlay.radius = CIRCLE_RADIUS_METER
+        circleOverlay.map = naverMap
     }
 
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
+        private const val CIRCLE_RADIUS_METER = 100.0
     }
 }
