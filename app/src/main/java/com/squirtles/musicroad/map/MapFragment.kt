bdd007225bc4a3588c6a835905d4994db9d92cb3
Ju Yungyeom <joo_930@naver.com>
2024-11-05 18:09:19 +0900

    [feature] 현재 내 위치 표시

--- app/src/main/java/com/squirtles/musicroad/map/MapFragment.kt
+++ app/src/main/java/com/squirtles/musicroad/map/MapFragment.kt
@@ -5,10 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
 import com.squirtles.musicroad.databinding.FragmentMapBinding
 
-class MapFragment : Fragment() {
 
+class MapFragment : Fragment(), OnMapReadyCallback {
+    private lateinit var locationSource: FusedLocationSource
+    private lateinit var naverMap: NaverMap
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
 
@@ -17,11 +24,29 @@ class MapFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentMapBinding.inflate(inflater, container, false)
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapView = binding.containerMap.getFragment<MapFragment>()
+        mapView.getMapAsync(this)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        this.naverMap = naverMap
+        naverMap.locationSource = locationSource
+        naverMap.uiSettings.isLocationButtonEnabled = true
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
