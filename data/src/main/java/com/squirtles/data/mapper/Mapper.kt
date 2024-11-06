8bc177ff39c84f9389d7a35c9dfa5aff065d944c
miller198 <milelr198@naver.com>
2024-11-06 21:21:42 +0900

    [chore] spotify api 통신 데이터클래스 패키징
    기존 data-model에서 data-model-spotify 패키지로 옮김

--- data/src/main/java/com/squirtles/data/mapper/Mapper.kt
+++ data/src/main/java/com/squirtles/data/mapper/Mapper.kt
@@ -1,6 +1,6 @@
 package com.squirtles.data.mapper
 
-import com.squirtles.data.datasource.remote.model.TempResponse
+import com.squirtles.data.datasource.remote.model.spotify.TempResponse
 import com.squirtles.domain.model.Temp
 
 internal fun TempResponse.toTemp(): Temp =
