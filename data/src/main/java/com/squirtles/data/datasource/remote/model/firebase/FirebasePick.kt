4a9cfddd582299ede33c69095698422448df965f
miller198 <milelr198@naver.com>
2024-11-06 23:22:30 +0900

    [refactor] FirebasePick @PropertyName 어노테이션
    Firestore에서 불러온 픽 정보 데이터클래스 변환 시 프로퍼티-필드명 같아야함. 어노테이션으로 이를 같게 만들수 있다

--- data/src/main/java/com/squirtles/data/datasource/remote/model/firebase/FirebasePick.kt
+++ data/src/main/java/com/squirtles/data/datasource/remote/model/firebase/FirebasePick.kt
@@ -1,23 +1,24 @@
 package com.squirtles.data.datasource.remote.model.firebase
 
 import com.google.firebase.firestore.GeoPoint
+import com.google.firebase.firestore.PropertyName
 import java.time.LocalDateTime
 
 /**
  * Firestore에 저장된 pick document를 불러와 변환하기위한 데이터 클래스
  */
 data class FirebasePick(
-    val id: String,
-    val albumTitle: String,
-    val artists: List<String>,
-    val songTitle: String,
-    val geoHash: String, // TODO: Firestore 데이터에도 추가해야함
-    val location: GeoPoint,
-    val comment: String,
-    val createdAt: LocalDateTime,
-    val createdBy: String,
-    val favoriteCount: Int = 0,
-    val imageUrl: String,
-    val previewUrl: String? = null,
-    val externalUrl: String,
+    @PropertyName("id") val id: String,
+    @PropertyName("album_title") val albumTitle: String,
+    @PropertyName("artists") val artists: List<String>,
+    @PropertyName("song_title") val songTitle: String,
+    @PropertyName("geo_hash") val geoHash: String, // TODO: Firestore 데이터에도 추가해야함
+    @PropertyName("location") val location: GeoPoint,
+    @PropertyName("comment") val comment: String,
+    @PropertyName("created_at") val createdAt: LocalDateTime,
+    @PropertyName("created_by") val createdBy: String,
+    @PropertyName("favorite_count") val favoriteCount: Int = 0,
+    @PropertyName("image_url") val imageUrl: String,
+    @PropertyName("preview_url") val previewUrl: String? = null,
+    @PropertyName("external_url") val externalUrl: String,
 )
