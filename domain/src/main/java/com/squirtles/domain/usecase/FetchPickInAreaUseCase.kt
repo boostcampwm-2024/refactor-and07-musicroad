cea804a91cf2a5cc1650d5c8c8a0a1103083c995
miller198 <milelr198@naver.com>
2024-11-06 23:20:58 +0900

    [feature] usecase 추가
    pick 한개 불러오는 유스케이스, 위치좌표와 반경 받아서 여러 pick 불러오는 유스케이스

---
+++ domain/src/main/java/com/squirtles/domain/usecase/FetchPickInAreaUseCase.kt
@@ -0,0 +1,11 @@
+package com.squirtles.domain.usecase
+
+import com.squirtles.domain.repository.FirebaseRepository
+import javax.inject.Inject
+
+class FetchPickInAreaUseCase @Inject constructor(
+    private val firebaseRepository: FirebaseRepository
+) {
+    suspend operator fun invoke(lat: Double, lng: Double, radiusInM: Double) =
+        firebaseRepository.fetchPicksInArea(lat, lng, radiusInM)
+}
