992d5b7d849939954984c2d2e784992dc417572d
miller198 <milelr198@naver.com>
2024-11-12 15:55:13 +0900

    [chore] import 누락

--- app/src/main/java/com/squirtles/musicroad/main/MainNavGraph.kt
+++ app/src/main/java/com/squirtles/musicroad/main/MainNavGraph.kt
@@ -5,9 +5,11 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import com.squirtles.musicroad.favorite.FavoriteScreen
 import com.squirtles.musicroad.map.MapScreen
 import com.squirtles.musicroad.map.MapViewModel
+import com.squirtles.musicroad.search2.SearchMusicScreen
 import com.squirtles.musicroad.setting.SettingScreen
 
 @Composable
@@ -27,7 +29,6 @@ fun MainNavGraph(
             MapScreen(
                 mapViewModel = mapViewModel,
                 onFavoriteClick = navigationActions.navigateToFavorite,
-                onSettingClick = navigationActions.navigateToSetting,
                 onCenterClick = { navController.navigate(CreatePickDestinations.CREATE_ROUTE) },
                 onSettingClick = navigationActions.navigateToSetting
             )
