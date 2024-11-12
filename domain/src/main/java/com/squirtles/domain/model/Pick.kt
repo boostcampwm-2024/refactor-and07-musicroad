54fca0efc46adbc791b6fe3127466cd9418980d3
Ju Yungyeom <joo_930@naver.com>
2024-11-13 00:15:34 +0900

    [design] 픽 상세화면

--- domain/src/main/java/com/squirtles/domain/model/Pick.kt
+++ domain/src/main/java/com/squirtles/domain/model/Pick.kt
@@ -1,5 +1,9 @@
 package com.squirtles.domain.model
 
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
 /**
  * 앱에서 사용하기 위한 Pick 정보 데이터클래스
  */
@@ -16,7 +20,15 @@ data class Pick(
     val imageUrl: String,
     val previewUrl: String,
     val externalUrl: String,
-)
+) {
+    val formattedDate: String
+        get() = formatTimestamp(createdAt)
+
+    private fun formatTimestamp(timestamp: Long): String {
+        val dateFormat = SimpleDateFormat("yyyy.MM.dd", Locale.getDefault())
+        return dateFormat.format(Date(timestamp))
+    }
+}
 
 data class GeoPoint(
     val latitude: Double,
