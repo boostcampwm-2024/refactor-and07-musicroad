e91bd367d655461c24090cf4bd1ba7e73158e654
miller198 <milelr198@naver.com>
2024-11-13 11:36:59 +0900

    [refactor] @SerialName annotation
    어노테이션 사용 일관성

--- data/src/main/java/com/squirtles/data/datasource/remote/applemusic/model/Data.kt
+++ data/src/main/java/com/squirtles/data/datasource/remote/applemusic/model/Data.kt
@@ -1,10 +1,9 @@
 package com.squirtles.data.datasource.remote.applemusic.model
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Data(
-    @SerialName("id") val id: String,
-    @SerialName("attributes") val attributes: Attributes,
+    val id: String,
+    val attributes: Attributes,
 )
