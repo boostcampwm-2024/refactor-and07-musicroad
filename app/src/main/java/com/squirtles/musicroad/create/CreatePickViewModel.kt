ff42de8c08f6ddf12b2f75b8a2265c431046e255
miller198 <miller198@naver.com>
2024-11-19 12:33:51 +0900

    [refactor] 리뷰 반영
    검색화면에서 필요 없는 curLocation 삭제, CreatePickViewModel에서 flow가 아닌 일반 Location 타입으로 curLocation 저장
    
    Co-Authored-By: minju <88606886+meanjoo@users.noreply.github.com>

--- app/src/main/java/com/squirtles/musicroad/create/CreatePickViewModel.kt
+++ app/src/main/java/com/squirtles/musicroad/create/CreatePickViewModel.kt
@@ -13,7 +13,6 @@ import com.squirtles.domain.usecase.SearchMusicVideoUseCase
 import com.squirtles.domain.usecase.SearchSongsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -41,7 +40,7 @@ class CreatePickViewModel @Inject constructor(
     private val _isSearching = MutableStateFlow<Boolean>(false)
     val isSearching = _isSearching.asStateFlow()
 
-    val curLocation: StateFlow<Location?> = fetchLocationUseCase()
+    val curLocation: Location? = fetchLocationUseCase().value
 
     fun searchSongs() {
         viewModelScope.launch {
--- app/src/main/java/com/squirtles/musicroad/create/SearchMusicScreen.kt
+++ app/src/main/java/com/squirtles/musicroad/create/SearchMusicScreen.kt
@@ -1,6 +1,5 @@
 package com.squirtles.musicroad.create
 
-import android.util.Log
 import android.util.Size
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -38,7 +37,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -77,11 +75,6 @@ fun SearchMusicScreen(
     val searchText by createPickViewModel.searchText.collectAsStateWithLifecycle()
     val searchResult by createPickViewModel.searchResult.collectAsStateWithLifecycle()
     val isSearching by createPickViewModel.isSearching.collectAsStateWithLifecycle()
-    val curLocation by createPickViewModel.curLocation.collectAsStateWithLifecycle()
-
-    LaunchedEffect(curLocation) {
-        Log.d("SearchMusicScreen", curLocation.toString())
-    }
 
     Scaffold(
         contentWindowInsets = WindowInsets.navigationBars,
