ab1f821a6deb875cf55796389ab116f25f442708
miller198 <miller198@naver.com>
2024-11-25 18:24:13 +0900

    [chore] PlayerState 파일 분리

---
+++ app/src/main/java/com/squirtles/musicroad/musicplayer/PlayerState.kt
@@ -0,0 +1,12 @@
+package com.squirtles.musicroad.musicplayer
+
+data class PlayerState(
+    val isReady: Boolean = true,
+    val isPlaying: Boolean = false,
+    val currentPosition: Long = 0L,
+) {
+    companion object {
+        val PLAYER_STATE_INITIAL = PlayerState(isReady = false)
+        val PLAYER_STATE_STOP = PlayerState(isReady = true, isPlaying = false)
+    }
+}
--- app/src/main/java/com/squirtles/musicroad/musicplayer/PlayerViewModel.kt
+++ app/src/main/java/com/squirtles/musicroad/musicplayer/PlayerViewModel.kt
@@ -18,17 +18,6 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-data class PlayerState(
-    val isReady: Boolean = true,
-    val isPlaying: Boolean = false,
-    val currentPosition: Long = 0L,
-) {
-    companion object {
-        val PLAYER_STATE_INITIAL = PlayerState(isReady = false)
-        val PLAYER_STATE_STOP = PlayerState(isReady = true, isPlaying = false)
-    }
-}
-
 @HiltViewModel
 class PlayerViewModel @Inject constructor() : ViewModel() {
 
