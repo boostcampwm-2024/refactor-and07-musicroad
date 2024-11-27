c607a9b062760f8b6531597b9ebe803b76665588
meanjoo <meanjooo@naver.com>
2024-11-27 17:57:16 +0900

    [refactor] BottomSheet가 사라진 후 화면 전환

--- app/src/main/java/com/squirtles/musicroad/map/components/ClusterBottomSheet.kt
+++ app/src/main/java/com/squirtles/musicroad/map/components/ClusterBottomSheet.kt
@@ -17,7 +17,9 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.SpanStyle
@@ -31,6 +33,7 @@ import com.squirtles.domain.model.Pick
 import com.squirtles.domain.model.Song
 import com.squirtles.musicroad.common.AlbumImage
 import com.squirtles.musicroad.create.HorizontalSpacer
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -41,11 +44,15 @@ fun ClusterBottomSheet(
     calculateDistance: (Double, Double) -> String,
     onClickItem: (String) -> Unit
 ) {
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+
     ModalBottomSheet(
         onDismissRequest = { onDismissRequest() },
         modifier = Modifier
             .fillMaxHeight()
             .padding(WindowInsets.statusBars.asPaddingValues()),
+        sheetState = sheetState,
         containerColor = MaterialTheme.colorScheme.surface
     ) {
         clusterPickList?.let { pickList ->
@@ -84,7 +91,11 @@ fun ClusterBottomSheet(
                         comment = pick.comment,
                         favoriteCount = pick.favoriteCount,
                         calculateDistance = calculateDistance,
-                        onClickItem = { onClickItem(pick.id) }
+                        onClickItem = {
+                            scope
+                                .launch { sheetState.hide() }
+                                .invokeOnCompletion { onClickItem(pick.id) }
+                        }
                     )
                 }
             }
