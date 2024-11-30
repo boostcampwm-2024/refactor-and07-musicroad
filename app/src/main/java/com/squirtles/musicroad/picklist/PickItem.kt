29fab21ff51f4bcda76fee9316394eecad112c9e
meanjoo <meanjooo@naver.com>
2024-12-01 04:02:41 +0900

    [refactor] LazyColumn 항목 파일 분리

---
+++ app/src/main/java/com/squirtles/musicroad/picklist/PickItem.kt
@@ -0,0 +1,91 @@
+package com.squirtles.musicroad.picklist
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ripple
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import com.squirtles.domain.model.Song
+import com.squirtles.musicroad.common.AlbumImage
+import com.squirtles.musicroad.common.CommentText
+import com.squirtles.musicroad.common.Constants
+import com.squirtles.musicroad.common.CreatedByOtherUserText
+import com.squirtles.musicroad.common.FavoriteCountText
+import com.squirtles.musicroad.common.HorizontalSpacer
+import com.squirtles.musicroad.common.SongInfoText
+import com.squirtles.musicroad.ui.theme.Gray
+import com.squirtles.musicroad.ui.theme.White
+
+@Composable
+internal fun PickItem(
+    song: Song,
+    createUserName: String,
+    favoriteCount: Int,
+    comment: String,
+    onItemClick: () -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = ripple(color = White),
+            ) { onItemClick() }
+            .padding(
+                horizontal = Constants.DEFAULT_PADDING,
+                vertical = Constants.DEFAULT_PADDING / 2
+            ),
+        horizontalArrangement = Arrangement.spacedBy(Constants.DEFAULT_PADDING),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        AlbumImage(
+            imageUrl = song.getImageUrlWithSize(Constants.REQUEST_IMAGE_SIZE_DEFAULT),
+            modifier = Modifier
+                .size(64.dp)
+                .clip(RoundedCornerShape(8.dp))
+        )
+
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            SongInfoText(
+                songInfo = "${song.songName} - ${song.artistName}",
+                color = White
+            )
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                CreatedByOtherUserText(
+                    userName = createUserName,
+                    modifier = Modifier.weight(weight = 1f, fill = false),
+                    color = Gray
+                )
+
+                HorizontalSpacer(8)
+
+                FavoriteCountText(
+                    favoriteCount = favoriteCount,
+                    iconTint = Gray,
+                    color = Gray
+                )
+            }
+
+            CommentText(
+                comment = comment,
+                color = Gray
+            )
+        }
+    }
+}
--- app/src/main/java/com/squirtles/musicroad/picklist/PickListScreen.kt
+++ app/src/main/java/com/squirtles/musicroad/picklist/PickListScreen.kt
@@ -1,50 +1,33 @@
 package com.squirtles.musicroad.picklist
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.wear.compose.material.CircularProgressIndicator
-import com.squirtles.domain.model.Song
 import com.squirtles.musicroad.R
-import com.squirtles.musicroad.common.AlbumImage
-import com.squirtles.musicroad.common.CommentText
 import com.squirtles.musicroad.common.Constants.COLOR_STOPS
 import com.squirtles.musicroad.common.Constants.DEFAULT_PADDING
-import com.squirtles.musicroad.common.Constants.REQUEST_IMAGE_SIZE_DEFAULT
-import com.squirtles.musicroad.common.CreatedByOtherUserText
 import com.squirtles.musicroad.common.DefaultTopAppBar
-import com.squirtles.musicroad.common.FavoriteCountText
-import com.squirtles.musicroad.common.HorizontalSpacer
-import com.squirtles.musicroad.common.SongInfoText
 import com.squirtles.musicroad.common.TotalCountText
-import com.squirtles.musicroad.ui.theme.Gray
 import com.squirtles.musicroad.ui.theme.Primary
 import com.squirtles.musicroad.ui.theme.White
 
@@ -134,66 +117,6 @@ fun PickListScreen(
                     // TODO: 다시하기 버튼 같은 거 만들어서 요청 다시 하게 할 수 있도록 만드는 것도 고려해보기
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun PickItem(
-    song: Song,
-    createUserName: String,
-    favoriteCount: Int,
-    comment: String,
-    onItemClick: () -> Unit,
-) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable(
-                interactionSource = remember { MutableInteractionSource() },
-                indication = ripple(color = White),
-            ) { onItemClick() }
-            .padding(horizontal = DEFAULT_PADDING, vertical = DEFAULT_PADDING / 2),
-        horizontalArrangement = Arrangement.spacedBy(DEFAULT_PADDING),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        AlbumImage(
-            imageUrl = song.getImageUrlWithSize(REQUEST_IMAGE_SIZE_DEFAULT),
-            modifier = Modifier
-                .size(64.dp)
-                .clip(RoundedCornerShape(8.dp))
-        )
-
-        Column(
-            modifier = Modifier.weight(1f)
-        ) {
-            SongInfoText(
-                songInfo = "${song.songName} - ${song.artistName}",
-                color = White
-            )
-
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                CreatedByOtherUserText(
-                    userName = createUserName,
-                    modifier = Modifier.weight(weight = 1f, fill = false),
-                    color = Gray
-                )
-
-                HorizontalSpacer(8)
-
-                FavoriteCountText(
-                    favoriteCount = favoriteCount,
-                    iconTint = Gray,
-                    color = Gray
-                )
-            }
-
-            CommentText(
-                comment = comment,
-                color = Gray
-            )
         }
     }
 }
