732b3e7997bb9c413e169d9a59c4b6ac80d82cca
meanjoo <meanjooo@naver.com>
2024-12-02 23:29:43 +0900

    [design] 권한 허용을 위한 설정 탭 이동 바 디자인

---
+++ app/src/main/java/com/squirtles/musicroad/main/PermissionBar.kt
@@ -0,0 +1,83 @@
+package com.squirtles.musicroad.main
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.squirtles.musicroad.R
+import com.squirtles.musicroad.common.Constants.DEFAULT_PADDING
+
+@Composable
+fun PermissionBar(
+    onClick: () -> Unit,
+) {
+    val context = LocalContext.current
+
+    Scaffold(
+        contentWindowInsets = WindowInsets.navigationBars
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(MaterialTheme.colorScheme.surface),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            ElevatedCard(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .padding(DEFAULT_PADDING),
+                shape = RoundedCornerShape(4.dp),
+                colors = CardDefaults.elevatedCardColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                ),
+                elevation = CardDefaults.elevatedCardElevation(
+                    defaultElevation = 4.dp
+                )
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = context.getString(R.string.snackbar_text),
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(start = DEFAULT_PADDING),
+                        color = MaterialTheme.colorScheme.onPrimaryContainer,
+                        overflow = TextOverflow.Ellipsis,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+
+                    TextButton(
+                        onClick = onClick,
+                        modifier = Modifier.padding(end = DEFAULT_PADDING / 2)
+                    ) {
+                        Text(
+                            text = context.getString(R.string.action_to_setting),
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
