bdc9cdd340861ee0707f5eb4c159aa34d241de6f
miller198 <miller198@naver.com>
2024-12-04 05:51:24 +0900

    [build] version 업그레이드

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.squirtles.musicroad"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 10100
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
