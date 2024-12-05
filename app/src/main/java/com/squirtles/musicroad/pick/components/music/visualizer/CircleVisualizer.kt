a77a7df3373066144df85e9c50e02d7654a0aba0
miller198 <miller198@naver.com>
2024-12-06 03:44:03 +0900

    [refactor] 오디오 가중치 수정

--- app/src/main/java/com/squirtles/musicroad/pick/components/music/visualizer/CircleVisualizer.kt
+++ app/src/main/java/com/squirtles/musicroad/pick/components/music/visualizer/CircleVisualizer.kt
@@ -68,22 +68,41 @@ private fun processAudioData(audioData: List<Float>): List<Float> {
     return normalizeAudioData(scaledData)
 }
 
-/* 주파수 대역별 가중치 */
+/* 주파수 대역별 가중치  */
 private fun scaleAudioData(audioData: List<Float>): List<Float> {
     val size = audioData.size
     return audioData.mapIndexed { index, value ->
-        val scaleFactor = if (index < size / 8) 0.5f
-        else if (index < size / 4) 1.0f // 저주파 대역
-        else if (index < size / 2) 1.5f // 중간 대역
-        else if (index < size / 1.5) 2.5f // 고주파 대역
-        else 4.0f // 고주파 대역
+        val scaleFactor = when {
+            index < size / 8 -> 0.4f
+            index < size / 4 -> 1.0f // 저주파 대역
+            index < size / 2 -> 2.0f // 중간 대역
+            index < size / 1.33 -> 3.0f // 고주파 대역
+            else -> 5.0f // 고주파 대역
+        }
         value * scaleFactor
     }
 }
 
-/* 0.0 ~ 1.0 사이 정규화  */
+private fun applyLogScale(audioData: List<Float>): List<Float> {
+    val epsilon = 1e-6f // 0 방지용 작은 값
+    val minValue = audioData.minOrNull() ?: 0f
+    val offset = if (minValue < 1f) 1f - minValue else 0f // 최소값을 1로 이동
+
+    return audioData.map { value ->
+        val shiftedValue = value + offset + epsilon // 데이터를 양수 범위로 이동
+        20 * kotlin.math.log10(shiftedValue) // 로그 변환
+    }
+}
+
+/* 다이나믹 레인지 압축 */
+private fun compressDynamicRangeRoot(audioData: List<Float>): List<Float> {
+    return audioData.map { kotlin.math.sqrt(it) }
+}
+
+/* 0.0 ~ 1.0 사이 정규화 */
 private fun normalizeAudioData(audioData: List<Float>): List<Float> {
     val max = audioData.maxOrNull() ?: 1f // 데이터 최대값
     val min = audioData.minOrNull() ?: 0f // 데이터 최소값
     return if (max - min <= 1f) List(audioData.size) { 0f } else audioData.map { (it - min) / (max - min) }
 }
+
